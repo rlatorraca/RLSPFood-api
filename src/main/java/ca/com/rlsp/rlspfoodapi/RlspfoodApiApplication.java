d16ab30a35085eb28953ffb2921d83acb4075e12
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-07-11 23:35:16 -0300

    First commit

---
+++ src/main/java/ca/com/rlsp/rlspfoodapi/RlspfoodApiApplication.java
@@ -0,0 +1,13 @@
+package ca.com.rlsp.rlspfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlspfoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RlspfoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/com/rlsp/rlspfoodapi/RlspfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.com.rlsp.rlspfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlspfoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
