617baf35160873c7a31321b7a436c3d0e307f217
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-07-14 18:12:10 -0300

    Implementing Content Negotiation (JSON and XML)

--- src/main/java/ca/com/rlsp/rlspfoodapi/api/controller/CuisineController.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/api/controller/CuisineController.java
@@ -3,6 +3,7 @@ package ca.com.rlsp.rlspfoodapi.api.controller;
 import ca.com.rlsp.rlspfoodapi.domain.model.Cuisine;
 import ca.com.rlsp.rlspfoodapi.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cuisines")
+@RequestMapping(name = "/cuisines", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class CuisineController {
 
     @Autowired
     public  CuisineRepository cuisineRepository;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<Cuisine> listAll(){
         return cuisineRepository.listAll();
     }
--- src/main/java/ca/com/rlsp/rlspfoodapi/api/controller/ProvinceController.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/api/controller/ProvinceController.java
@@ -3,6 +3,7 @@ package ca.com.rlsp.rlspfoodapi.api.controller;
 import ca.com.rlsp.rlspfoodapi.domain.model.Province;
 import ca.com.rlsp.rlspfoodapi.domain.repository.ProvinceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,13 @@ public class ProvinceController {
     @Autowired
     private ProvinceRepository provinceRepository;
 
-    @GetMapping
-    public List<Province> listAll(){
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Province> listAllJson(){
+        return provinceRepository.listAll();
+    }
+
+    @GetMapping(produces = { MediaType.APPLICATION_XML_VALUE})
+    public List<Province> listAllXml(){
         return provinceRepository.listAll();
     }
 }
