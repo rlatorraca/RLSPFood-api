7d4d29c05b0be23baeff5089c83afa02466b46ea
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-07-16 16:22:42 -0300

    Exemples of ResponseEntity<> return

--- src/main/java/ca/com/rlsp/rlspfoodapi/api/controller/CuisineController.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/api/controller/CuisineController.java
@@ -6,6 +6,7 @@ import ca.com.rlsp.rlspfoodapi.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +26,15 @@ public class CuisineController {
 
     @ResponseStatus(HttpStatus.OK) // Codigo de Resposta do Servidor quue sera enviado para essa requisaicao
     @GetMapping("/{cuisineId}")
-    public Cuisine findBy1Id(@PathVariable("cuisineId") Long id){
-        return cuisineRepository.findById(id);
+    public ResponseEntity<Cuisine> findBy1Id(@PathVariable("cuisineId") Long id){
+        Cuisine cuisine =  cuisineRepository.findById(id);
+
+        //return ResponseEntity.ok(cuisine);
+        if(cuisine != null){
+            return ResponseEntity.status(HttpStatus.OK).body(cuisine);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
     }
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
