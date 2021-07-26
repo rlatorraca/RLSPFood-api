11bda9b4c3fb384f5999753c427d7d114c01be1f
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-07-26 12:36:14 -0300

    Implemented Mapping @OneToMany on Cuisine

--- src/main/java/ca/com/rlsp/rlspfoodapi/domain/model/Cuisine.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/domain/model/Cuisine.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 //@JsonRootName("gastronomy") // Mudar o nome do Ckasse em XML para outro nome
@@ -21,9 +23,14 @@ public class Cuisine {
     @EqualsAndHashCode.Include
     private Long id;
 
-    //@JsonIgnore // remove da Apresentacao da Classe de Dominio na resposta
+
     @JsonProperty("nome") // Muda o nome que vira no JSON quando consultado (diferentemente da Classe de Dominio)
     @Column(nullable = false, name="cuisine_name")
     private String name;
+
+    @JsonIgnore /// remove da Apresentacao da Classe de Dominio na resposta (no Json de respota)
+    @OneToMany(mappedBy = "cuisinePode ")
+    private List<Restaurant> restaurants = new ArrayList<>();
+
 
 }
