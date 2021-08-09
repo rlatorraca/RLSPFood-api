b69a6f858e5122dd3b03ae35759a4ab6447f7074
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-08-09 19:15:02 -0300

    Firsts Integration Testisn Cuisine in Use and Cuisine Inexistent

--- src/test/java/ca/com/rlsp/rlspfoodapi/CuisineRegistrationServiceTests.java
+++ src/test/java/ca/com/rlsp/rlspfoodapi/CuisineRegistrationServiceTests.java
@@ -4,8 +4,11 @@ package ca.com.rlsp.rlspfoodapi;
     TESTE DE INTEGRACAO
  */
 
+import ca.com.rlsp.rlspfoodapi.domain.exception.CuisineNotFoundException;
+import ca.com.rlsp.rlspfoodapi.domain.exception.EntityIsForeignKeyException;
 import ca.com.rlsp.rlspfoodapi.domain.model.Cuisine;
 import ca.com.rlsp.rlspfoodapi.domain.service.CuisineRegistrationService;
+import com.fasterxml.jackson.annotation.OptBoolean;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintViolationException;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +31,7 @@ public class CuisineRegistrationServiceTests {
     private CuisineRegistrationService cuisineRegistrationService;
 
     @Test
-    public void given_DoingRegistrationCuisineCorrectly_ThenSuccess(){
+    public void when_RegisteringCuisineCorrectly_ThenSuccess() {
 
         // CENARIO
         Cuisine newCuisine = new Cuisine();
@@ -41,20 +46,49 @@ public class CuisineRegistrationServiceTests {
     }
 
     @Test
-    public void given_RegistrationCuisineNoName_ThenFail(){
+    public void when_RegisteringCuisineNoName_ThenFail() {
 
         // CENARIO
         Cuisine newCuisine = new Cuisine();
         newCuisine.setName(null);
 
         // ACAO
-        ConstraintViolationException exceptionxpected = Assertions.assertThrows(ConstraintViolationException.class,() ->{
-           cuisineRegistrationService.save(newCuisine);
+        ConstraintViolationException exceptionxpected = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            cuisineRegistrationService.save(newCuisine);
         });  // Espera-se ocorrer um Exception
 
         //VALIDACAO
         assertThat(exceptionxpected).isNotNull();
         assertThat(exceptionxpected).isInstanceOf(ConstraintViolationException.class);
 
+    }
+
+    @Test
+    public void when_TryingExcludeCuisineInUse_Then_Fail() {
+
+        // CENARIO
+        Optional<Cuisine> cuisine = cuisineRegistrationService.findById(1L);
+        // ACAO
+        EntityIsForeignKeyException exceptionxpected = Assertions.assertThrows(EntityIsForeignKeyException.class, () -> {
+            cuisineRegistrationService.remove(cuisine.get().getId());
+        });
+
+        // VALIDACAO
+            assertThat(exceptionxpected).isNotNull();
+
+    }
+
+    @Test
+    public void when_TryingExcludeCuisineInexistent_Then_Fail() {
+
+        /// CENARIO
+
+        // ACAO
+        CuisineNotFoundException exceptionxpected = Assertions.assertThrows(CuisineNotFoundException.class, () -> {
+            cuisineRegistrationService.remove(1000L);
+        });
+
+        // VALIDACAO
+        assertThat(exceptionxpected).isNotNull();
     }
 }
