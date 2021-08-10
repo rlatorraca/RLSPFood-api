e658513571b8c6f8f0652bb752b6fd4396b8457e
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-08-10 12:36:23 -0300

    Added test to validades response body getting All Cuisines

--- src/test/java/ca/com/rlsp/rlspfoodapi/CuisineAPITestsIT.java
+++ src/test/java/ca/com/rlsp/rlspfoodapi/CuisineAPITestsIT.java
@@ -4,6 +4,8 @@ import static io.restassured.RestAssured.given;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,8 @@ public class CuisineAPITestsIT {
 
     @LocalServerPort // Como estamos usando RANDOM_PORT (porta aleatoria) temos que pegar essa porta para entao fazermos a conexao com o servidor WEB mock
     private int randomPort;
+
+    // Valida o Codigo da Resposta
     @Test
     public void must_ReturnStatus200_whenQueryAllCuisines(){
 
@@ -30,5 +34,23 @@ public class CuisineAPITestsIT {
             .get()
         .then()
             .statusCode(HttpStatus.OK.value());
+    }
+
+    // Valida o Corpo da Resposta
+    @Test
+    public void must_ReturnNineCuisines_whenQueryAllCuisines(){
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Quando falar mostrar REQUISICAO e REPOSTA
+
+        given()
+            .basePath("/cuisines")
+            .port(randomPort)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .body("", Matchers.hasSize(9))
+            .body("nome", Matchers.hasItems("Portuguese", "Haule"));
+
     }
 }
