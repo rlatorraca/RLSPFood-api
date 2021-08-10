124d31e08b2f33c1ce8615c8dbd33089fac5b1e3
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-08-10 12:13:58 -0300

    Added RestAssured and built first API test

---
+++ src/test/java/ca/com/rlsp/rlspfoodapi/CuisineAPITestsIT.java
@@ -0,0 +1,34 @@
+package ca.com.rlsp.rlspfoodapi;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //Levanta um Servidor Web para testar
+public class CuisineAPITestsIT {
+
+    @LocalServerPort // Como estamos usando RANDOM_PORT (porta aleatoria) temos que pegar essa porta para entao fazermos a conexao com o servidor WEB mock
+    private int randomPort;
+    @Test
+    public void must_ReturnStatus200_whenQueryAllCuisines(){
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Quando falar mostrar REQUISICAO e REPOSTA
+
+        given()
+            .basePath("/cuisines")
+            .port(randomPort)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+}
