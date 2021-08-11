8a024fd82ba2f1466734b96fe9e86cdd3696ae60
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-08-11 18:53:10 -0300

    Working on Restaurant Test. Needs change something on must_ReturnStatus400_WhenRegistringRestaurantInvalidCuisine

--- src/test/java/ca/com/rlsp/rlspfoodapi/CuisineAPITestsIT.java
+++ src/test/java/ca/com/rlsp/rlspfoodapi/CuisineAPITestsIT.java
@@ -6,11 +6,10 @@ import static org.hamcrest.Matchers.equalTo;
 import ca.com.rlsp.rlspfoodapi.domain.model.Cuisine;
 import ca.com.rlsp.rlspfoodapi.domain.repository.CuisineRepository;
 import ca.com.rlsp.rlspfoodapi.util.DatabaseCleaner;
+import ca.com.rlsp.rlspfoodapi.util.ResourcesUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.aspectj.lang.annotation.Before;
 import org.flywaydb.core.Flyway;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +25,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //Levanta um Servidor Web para testar
 @TestPropertySource("/application_test.properties") // Para fazer as mudancas necessarias para o DB de tests
 public class CuisineAPITestsIT {
+
+    private static final int CUISINE_ID_INEXISTENT = 1000000;
+    private Cuisine canadianCuisine;
+    private int totalRegisteredCuisines;
+    private String jsonOneCuisine;
 
     @LocalServerPort // Como estamos usando RANDOM_PORT (porta aleatoria) temos que pegar essa porta para entao fazermos a conexao com o servidor WEB mock
     private int randomPort;
@@ -46,34 +50,19 @@ public class CuisineAPITestsIT {
      */
     @BeforeEach
     public void setUp(){
-       RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Quando falar mostrar REQUISICAO e REPOSTA
+       RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); // Quando falhar mostrara: REQUISICAO e RESPOSTA
        RestAssured.port = randomPort;
        RestAssured.basePath = "/cuisines";
 
        //flyway.migrate();
-        
-        // Limpa e prapra os dados a cada teste
-        databaseCleaner.clearTables();
-        prepareDataForTesting();
-        
+       // Limpa e prapra os dados a cada teste
+       databaseCleaner.clearTables();
+       prepareDataForTesting();
+
+       jsonOneCuisine = ResourcesUtils.getContentFromResource("/json/cuisine/correct/oneCuisine.json");
     }
 
-    private void prepareDataForTesting() {
-
-        Cuisine cuisine01 = new Cuisine();
-        cuisine01.setName("Canadian");
-        cuisineRepository.save(cuisine01);
-
-        Cuisine cuisine02 = new Cuisine();
-        cuisine02.setName("American");
-        cuisineRepository.save(cuisine02);
-
-        Cuisine cuisine03 = new Cuisine();
-        cuisine03.setName("Chinese");
-        cuisineRepository.save(cuisine03);
-    }
-
-    // Valida o Codigo da Resposta 200 ao buscar todas cozinhas//
+    // Valida o Codigo da Resposta 200 ao buscar todas cozinhas
     @Test
     public void must_ReturnStatus200_whenQueryAllCuisines(){
 
@@ -84,13 +73,13 @@ public class CuisineAPITestsIT {
         .then()
             .statusCode(HttpStatus.OK.value());
     }
-
     // Valida o Codigo da Resposta 201 ao criar uma cozinha
+
     @Test
     public void must_ReturnStatus201_whenCreatedCuissine(){
 
         given()
-            .body("{ \"nome\":\"Irish\" }")
+            .body(jsonOneCuisine)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
         .when()
@@ -98,42 +87,42 @@ public class CuisineAPITestsIT {
         .then()
              .statusCode(HttpStatus.CREATED.value());
     }
-
     // Valida o Corpo da Resposta
+
     @Test
-    public void must_ReturnNineCuisines_whenQueryAllCuisines(){
+    public void must_ReturnTotalCuisines_whenQueryAllCuisines(){
 
         given()
             .accept(ContentType.JSON)
         .when()
             .get()
         .then()
-            .body("", Matchers.hasSize(3))
+            .body("", Matchers.hasSize(totalRegisteredCuisines))
             .body("nome", Matchers.hasItems("American", "Chinese"));
 
     }
-
     // Testa passando parametros (Retornando STATUS CODE 200 e nome da cozinha
     // GET /cuisine/{id}
+
     @Test
     public void must_ReturnStatusAndOneCuisine_whenSentIdAndExistent(){
         given()
-            .pathParam("cuisineId", 3)
+            .pathParam("cuisineId", canadianCuisine.getId())
             .accept(ContentType.JSON)
         .when()
             .get("/{cuisineId}")
         .then()
              .statusCode(HttpStatus.OK.value())
-             .body("nome", equalTo("Chinese"));
+             .body("nome", equalTo(canadianCuisine.getName()));
     }
-
     // Testa passando parametros (Retornando STATUS CODE and
     // GET /cuisine/{id}
+
     @Test
     public void must_ReturnStatus404_whenQueryInexistentCuisine(){
 
         given()
-            .pathParam("cuisineId", 33)
+            .pathParam("cuisineId", CUISINE_ID_INEXISTENT)
             .accept(ContentType.JSON)
         .when()
             .get("/{cuisineId}")
@@ -141,4 +130,22 @@ public class CuisineAPITestsIT {
             .statusCode(HttpStatus.NOT_FOUND.value())
             .body("nome", equalTo(null));
     }
+
+    private void prepareDataForTesting() {
+
+        canadianCuisine = new Cuisine();
+        canadianCuisine.setName("Canadian");
+        cuisineRepository.save(canadianCuisine);
+
+        Cuisine cuisine02 = new Cuisine();
+        cuisine02.setName("American");
+        cuisineRepository.save(cuisine02);
+
+        Cuisine cuisine03 = new Cuisine();
+        cuisine03.setName("Chinese");
+        cuisineRepository.save(cuisine03);
+        totalRegisteredCuisines = (int) cuisineRepository.count();
+    }
+
+
 }
