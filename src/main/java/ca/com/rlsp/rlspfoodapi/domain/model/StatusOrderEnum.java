040e939c8f9665d22192891677805efbd6a2e24f
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-09-08 23:46:14 -0300

    StatusOrderEnum modified

--- src/main/java/ca/com/rlsp/rlspfoodapi/domain/model/StatusOrderEnum.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/domain/model/StatusOrderEnum.java
@@ -6,9 +6,10 @@ import lombok.Getter;
 public enum StatusOrderEnum {
 
     CREATED("Order Created"),
-    CONFIRMED_START("Order Started"),
-    CONFIRMED_OVEN("Order into oven"),
-    CONFIRMED_READY("Order ready to go"),
+    CONFIRMED("Order Confirmed"),
+    STARTED("Order Started"),
+    ON_THE_OVEN("Order on the oven"),
+    READY("Order ready to delivery"),
     ON_THE_ROAD("Order on the road"),
     DELIVERED("Order Delivered"),
     CANCELED("Order Canceled");
