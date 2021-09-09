656972c6821716f3b370c063262d68b07959b376
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-09-09 00:39:49 -0300

    StatusOrderRegistrationService added all Status

--- src/main/java/ca/com/rlsp/rlspfoodapi/domain/service/StatusOrderRegistrationService.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/domain/service/StatusOrderRegistrationService.java
@@ -12,7 +12,13 @@ import java.time.OffsetDateTime;
 @Service
 public class StatusOrderRegistrationService {
 
-    public static final String MSG_STATUS_ORDER_CANNOT_BE_CONFIRMED="Order Status %d cannot be changed from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_CONFIRMED="Order Status %d cannot be modified from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_CANCELED="Order Status %d cannot be canceled from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_STARTED="Order Status %d cannot be started from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_ON_THE_OVEN="Order Status %d cannot be on the oven from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_TO_READY="Order Status %d cannot be ready from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_ON_THE_ROAD="Order Status %d cannot be on the road from %s to %s";
+    public static final String MSG_STATUS_ORDER_CANNOT_BE_DELIVERED="Order Status %d cannot be delivered from %s to %s";
 
     @Autowired
     private IssueOfOrderRegistrationService issueOfOrderRegistrationService;
@@ -32,6 +38,114 @@ public class StatusOrderRegistrationService {
         }
 
         order.setStatus(StatusOrderEnum.CONFIRMED);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void toCancel(Long orderId) {
+        Order order = issueOfOrderRegistrationService.findOrFail(orderId);
+
+        if(!(order.getStatus().equals(StatusOrderEnum.CONFIRMED))) {
+            throw new GenericBusinessException(
+                    String.format(
+                            MSG_STATUS_ORDER_CANNOT_BE_CANCELED,
+                            order.getId(),
+                            order.getStatus().getDescription(),
+                            StatusOrderEnum.CANCELED.getDescription())
+            );
+        }
+
+        order.setStatus(StatusOrderEnum.CANCELED);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void toStart(Long orderId) {
+        Order order = issueOfOrderRegistrationService.findOrFail(orderId);
+
+        if(!(order.getStatus().equals(StatusOrderEnum.CONFIRMED))) {
+            throw new GenericBusinessException(
+                    String.format(
+                            MSG_STATUS_ORDER_CANNOT_BE_STARTED,
+                            order.getId(),
+                            order.getStatus().getDescription(),
+                            StatusOrderEnum.STARTED.getDescription())
+            );
+        }
+
+        order.setStatus(StatusOrderEnum.STARTED);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void toOnTheOven(Long orderId) {
+        Order order = issueOfOrderRegistrationService.findOrFail(orderId);
+
+        if(!(order.getStatus().equals(StatusOrderEnum.STARTED))) {
+            throw new GenericBusinessException(
+                    String.format(
+                            MSG_STATUS_ORDER_CANNOT_BE_ON_THE_OVEN,
+                            order.getId(),
+                            order.getStatus().getDescription(),
+                            StatusOrderEnum.ON_THE_OVEN.getDescription())
+            );
+        }
+
+        order.setStatus(StatusOrderEnum.ON_THE_OVEN);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void toReady(Long orderId) {
+        Order order = issueOfOrderRegistrationService.findOrFail(orderId);
+
+        if(!(order.getStatus().equals(StatusOrderEnum.ON_THE_OVEN))) {
+            throw new GenericBusinessException(
+                    String.format(
+                            MSG_STATUS_ORDER_CANNOT_BE_TO_READY,
+                            order.getId(),
+                            order.getStatus().getDescription(),
+                            StatusOrderEnum.READY.getDescription())
+            );
+        }
+
+        order.setStatus(StatusOrderEnum.READY);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void toOnTheRoad(Long orderId) {
+        Order order = issueOfOrderRegistrationService.findOrFail(orderId);
+
+        if(!(order.getStatus().equals(StatusOrderEnum.READY))) {
+            throw new GenericBusinessException(
+                    String.format(
+                            MSG_STATUS_ORDER_CANNOT_BE_ON_THE_ROAD,
+                            order.getId(),
+                            order.getStatus().getDescription(),
+                            StatusOrderEnum.ON_THE_ROAD.getDescription())
+            );
+        }
+
+        order.setStatus(StatusOrderEnum.ON_THE_ROAD);
+        order.setConfirmationDate(OffsetDateTime.now());
+    }
+
+    @Transactional
+    public void toDelivered(Long orderId) {
+        Order order = issueOfOrderRegistrationService.findOrFail(orderId);
+
+        if(!(order.getStatus().equals(StatusOrderEnum.ON_THE_ROAD))) {
+            throw new GenericBusinessException(
+                    String.format(
+                            MSG_STATUS_ORDER_CANNOT_BE_DELIVERED,
+                            order.getId(),
+                            order.getStatus().getDescription(),
+                            StatusOrderEnum.DELIVERED.getDescription())
+            );
+        }
+
+        order.setStatus(StatusOrderEnum.DELIVERED);
         order.setConfirmationDate(OffsetDateTime.now());
     }
 }
