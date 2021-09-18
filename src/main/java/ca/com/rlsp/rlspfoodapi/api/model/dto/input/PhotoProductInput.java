5ae7a40ad147a3ee2f0876460d781f8ca83b7cff
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-09-18 14:02:29 -0300

    Build a validator to validate photo content type (JPG or PNG)

--- src/main/java/ca/com/rlsp/rlspfoodapi/api/model/dto/input/PhotoProductInput.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/api/model/dto/input/PhotoProductInput.java
@@ -1,8 +1,11 @@
 package ca.com.rlsp.rlspfoodapi.api.model.dto.input;
 
+
+import ca.com.rlsp.rlspfoodapi.core.validation.FileContentType;
 import ca.com.rlsp.rlspfoodapi.core.validation.PhotoSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -14,6 +17,7 @@ public class PhotoProductInput {
 
     @NotNull
     @PhotoSize(max="500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE} )
     private MultipartFile file;
     @NotBlank
     private String description;
