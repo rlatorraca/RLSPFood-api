9457cf6a661dbf035095d22239d6eb71360a67bb
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-10-01 13:58:39 -0300

    Add some technical information about the RLSPFood

--- src/main/java/ca/com/rlsp/rlspfoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/core/openapi/SpringFoxConfig.java
@@ -1,12 +1,18 @@
 package ca.com.rlsp.rlspfoodapi.core.openapi;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -27,11 +33,22 @@ public class SpringFoxConfig implements WebMvcConfigurer {
         return new
                 Docket(DocumentationType.OAS_30)
                     .select() // seleciona os endpoints que serao expostos
-                    //.apis(RequestHandlerSelectors.any()) // tudo relacionado a API sera incluido (inlcusive do SpringBoot
-                    .apis(RequestHandlerSelectors.basePackage("ca.com.rlsp.rlspfoodapi.api"))
-                    .paths(PathSelectors.any())
-                    //.paths(PathSelectors.ant("/restaurants/*")) // apenas o que tiver dentro de restaurnt vai ser mostrado
-                    .build();
+                        //.apis(RequestHandlerSelectors.any()) // tudo relacionado a API sera incluido (inlcusive do SpringBoot
+                        .apis(RequestHandlerSelectors.basePackage("ca.com.rlsp.rlspfoodapi.api"))
+                        .paths(PathSelectors.any())
+                        //.paths(PathSelectors.ant("/restaurants/*")) // apenas o que tiver dentro de restaurnt vai ser mostrado
+                        .build()
+                .apiInfo(rlspApiInfo());
+    }
+
+    public ApiInfo rlspApiInfo(){
+        return new ApiInfoBuilder()
+                .title("RLSP FOOD API")
+                .description("API for Canada and Maritimes restaurants")
+                .version("1.34")
+                .contact(new Contact("RLSPFood", "https://www.rlspfood.api.com.ca", "contact@rlspfood.api.com.ca"))
+                .build();
+
     }
 
 
