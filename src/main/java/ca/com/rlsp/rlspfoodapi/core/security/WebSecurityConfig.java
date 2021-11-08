8e94c84b82aebc479ee6f05563d3c5d1141bba53
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-11-07 22:48:51 -0400

    Setups to Memory User Authentication

--- src/main/java/ca/com/rlsp/rlspfoodapi/core/security/WebSecurityConfig.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/core/security/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package ca.com.rlsp.rlspfoodapi.core.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Setup the Spring Security on the API
@@ -29,5 +33,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Configura sessao sem ESTADO (sem cookies)
                 .and()
                     .csrf().disable(); // Cross-Site Request Forgery (CSRF) (no more cookies in the API)
+    }
+
+    /**
+     * Memory User Authentication
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                    .password(passwordEncoder().encode("12345"))
+                    .roles("ADMIN")
+                .and()
+                .withUser("rlsp")
+                    .password(passwordEncoder().encode("1234"))
+                    .roles("ADMIN");
+    }
+
+    /**
+     * Password cryptography using BCrypt Encoder
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
