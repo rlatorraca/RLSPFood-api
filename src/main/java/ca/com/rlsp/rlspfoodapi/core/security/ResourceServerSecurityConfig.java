9d69dac5a8775ace3a99adedc217156c3b00ea4f
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-11-09 22:13:20 -0400

    Setups to run a Resource Server using Spring Security stack

--- src/main/java/ca/com/rlsp/rlspfoodapi/core/security/ResourceServerSecurityConfig.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/core/security/ResourceServerSecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Setup the Spring Security on the API
  */
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * Retira a tela de Login web a inicia a configuracao de autorizacao de acesso nas requisicoes
@@ -22,22 +22,41 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .oauth2ResourceServer() // Habilita um "Resource Server" na API
+                .opaqueToken();  // opaqueToken (sem possibilidade de leitura <> do JWT (possivel leitura))
+    }
+
+    /**
+     *  OLD ONE
+     * Retira a tela de Login web a inicia a configuracao de autorizacao de acesso nas requisicoes
+     * @param http
+     * @throws Exception
+     */
+    /**
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
                 .and()
-                    .authorizeRequests()
-                        .antMatchers("/v1/cuisines/**").permitAll() // permite esse endpoint sem Autenticacao
-                        .anyRequest().authenticated() // autoriza qualquer autorizacao autenticada
+                .authorizeRequests()
+                .antMatchers("/v1/cuisines/**").permitAll() // permite esse endpoint sem Autenticacao
+                .anyRequest().authenticated() // autoriza qualquer autorizacao autenticada
 
                 .and()
-                    .sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Configura sessao sem ESTADO (sem cookies)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Configura sessao sem ESTADO (sem cookies)
                 .and()
-                    .csrf().disable(); // Cross-Site Request Forgery (CSRF) (no more cookies in the API)
+                .csrf().disable(); // Cross-Site Request Forgery (CSRF) (no more cookies in the API)
     }
+    */
 
     /**
      * Memory User Authentication
      */
+    /** FEITO PELO "RLSPFood-api-Auth" (Authorization Server)
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -54,8 +73,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      * Password cryptography using BCrypt Encoder
      * @return
      */
+    /**
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+      */
+
 }
