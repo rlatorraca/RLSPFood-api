3770bb79a6c07b1ae8872c8411ec8d74d47600c7
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-11-26 00:22:55 -0400

    Modify RlspFoodSecurity to generate links dynamically based on the permission

--- src/main/java/ca/com/rlsp/rlspfoodapi/core/security/RlspFoodSecurity.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/core/security/RlspFoodSecurity.java
@@ -37,19 +37,114 @@ public class RlspFoodSecurity {
         return orderRepository.isOrderManagedFor(orderCode, getUserId());
     }
 
+
+    /**
+     * Check if an Authenticated user is the same that requested the action
+     * @param userId
+     * @return
+     */
     public boolean userAuthenticatedAndEqualUserPassed(Long userId){
         return getUserId() != null && userId != null && getUserId() == userId;
     }
 
+    /**
+     * Verify the authorities existing for that user
+     * @param authorityName
+     * @return
+     */
     public boolean hasAuthority(String authorityName) {
         return getAuthentication().getAuthorities().stream()
                 .anyMatch(authority -> authority.getAuthority().equals(authorityName));
     }
 
+    /**
+     * Used to know if user has permission to modify status of an order
+     * @param orderCode
+     * @return
+     */
     public boolean hasPermissionToModifyOrderStatus(String orderCode){
         return hasAuthority("SCOPE_WRITE")
                 && (
                 hasAuthority("EDIT_ORDERS") || manageRestaurantOrders(orderCode));
+    }
+
+    /**
+     * Check if user is Authenticated
+     * @return
+     */
+    public boolean isAuthenticated() {
+        return getAuthentication().isAuthenticated();
+    }
+
+    /**
+     * Check if user has SCOPE_WRITE
+     * @return
+     */
+    public boolean hasWriteScope() {
+        return hasAuthority("SCOPE_WRITE");
+    }
+
+    /**
+     * Check if user has SCOPE_READ
+     * @return
+     */
+    public boolean hasReadScope() {
+        return hasAuthority("SCOPE_READ");
+    }
+
+    /**
+     * Methods to check the scope and permissions
+     * @return
+     */
+    public boolean hasPermissionToQueryRestaurants() {
+        return hasReadScope() && isAuthenticated();
+    }
+
+    public boolean hasPermissionToManageRestaurantRegister () {
+        return hasWriteScope() && hasAuthority("EDIT_RESTAURANTS");
+    }
+
+    public boolean hasPermissionToManageOpenCloseRestaurants (Long restauranteId) {
+        return hasWriteScope() && (hasAuthority("EDITAR_RESTAURANTES")
+                || manageRestaurant(restauranteId));
+    }
+
+    public boolean hasPermissionToQueryUsersGroupsPermissions () {
+        return hasReadScope() && hasAuthority("QUERY_USERS_GROUPS_PERMISSIONS");
+    }
+
+    public boolean hasPermissionToEditUsersGroupsPermissions() {
+        return hasWriteScope() && hasAuthority("EDIT_USERS_GROUPS_PERMISSIONS");
+    }
+
+    public boolean hasPermissionToQueryOrders(Long clienteId, Long restauranteId) {
+        return hasReadScope() && (hasAuthority("QUERY_ORDERS")
+                || userAuthenticatedAndEqualUserPassed(clienteId)
+                || manageRestaurant(restauranteId));
+    }
+
+    public boolean hasPermissionToQueryOrders() {
+        return isAuthenticated() && hasReadScope();
+    }
+
+    public boolean hasPermissionToQueryPaymentTypes() {
+        return isAuthenticated() && hasReadScope();
+    }
+
+    public boolean hasPermissionToQueryCities () {
+        return isAuthenticated() && hasReadScope();
+    }
+
+    public boolean hasPermissionToQueryProvinces () {
+        return isAuthenticated() && hasWriteScope();
+    }
+
+    public boolean hasPermissionToQueryCuisines () {
+        return isAuthenticated() && hasReadScope();
+    }
+
+    public boolean hasPermissionToQueryStatistics () {
+        return hasReadScope() && hasAuthority("GENERATE_STATISTICS");
     }
 
 }
