b67578eea86a11192eabcc50bccc6aa8dc9e0069
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-11-27 00:18:50 -0400

    Alter method toModel on GrupoModelAssembler to verify permission to have access.

--- src/main/java/ca/com/rlsp/rlspfoodapi/api/v1/assembler/GroupModelAssembler.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/api/v1/assembler/GroupModelAssembler.java
@@ -4,6 +4,7 @@ import ca.com.rlsp.rlspfoodapi.api.v1.controller.GroupController;
 import ca.com.rlsp.rlspfoodapi.api.v1.links.BuildLinks;
 import ca.com.rlsp.rlspfoodapi.api.v1.model.dto.input.GroupInputDto;
 import ca.com.rlsp.rlspfoodapi.api.v1.model.dto.output.GroupOutputDto;
+import ca.com.rlsp.rlspfoodapi.core.security.RlspFoodSecurity;
 import ca.com.rlsp.rlspfoodapi.domain.model.Group;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,9 @@ public class GroupModelAssembler extends RepresentationModelAssemblerSupport<Gro
 
     @Autowired
     private BuildLinks buildLinks;
+
+    @Autowired
+    private RlspFoodSecurity rlspFoodSecurity;
 
 
     public GroupModelAssembler() {
@@ -66,15 +70,22 @@ public class GroupModelAssembler extends RepresentationModelAssemblerSupport<Gro
         GroupOutputDto groupOutputDto = createModelWithId(group.getId(), group);
         modelMapper.map(group, groupOutputDto);
 
-        groupOutputDto.add(buildLinks.getLinkToGroups("groups"));
-        groupOutputDto.add(buildLinks.getLinkToGroupPermissions(group.getId(), "permissions"));
+        if(rlspFoodSecurity.hasPermissionToQueryUsersGroupsPermissions()){
+            groupOutputDto.add(buildLinks.getLinkToGroups("groups"));
+            groupOutputDto.add(buildLinks.getLinkToGroupPermissions(group.getId(), "permissions"));
+        }
 
         return groupOutputDto;
     }
 
     @Override
     public CollectionModel<GroupOutputDto> toCollectionModel(Iterable<? extends Group> groups) {
-        return super.toCollectionModel(groups)
-                .add(buildLinks.getLinkToGroups());
+        CollectionModel<GroupOutputDto> collectionModel = super.toCollectionModel(groups);
+
+        if(rlspFoodSecurity.hasPermissionToQueryUsersGroupsPermissions()){
+            collectionModel.add(buildLinks.getLinkToGroups());
+        }
+
+        return collectionModel;
     }
 }
