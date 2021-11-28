cf53b89ce53142aca043bacf6b6255db2c917c7a
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-11-28 00:05:58 -0400

    Alter method listAll into RootEntryPointController to verify permission to access

--- src/main/java/ca/com/rlsp/rlspfoodapi/api/v1/controller/RootEntryPointController.java
+++ src/main/java/ca/com/rlsp/rlspfoodapi/api/v1/controller/RootEntryPointController.java
@@ -1,6 +1,7 @@
 package ca.com.rlsp.rlspfoodapi.api.v1.controller;
 
 import ca.com.rlsp.rlspfoodapi.api.v1.links.BuildLinks;
+import ca.com.rlsp.rlspfoodapi.core.security.RlspFoodSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.MediaType;
@@ -16,20 +17,47 @@ public class RootEntryPointController {
     @Autowired
     private BuildLinks buildLinks;
 
+    @Autowired
+    private RlspFoodSecurity rlspFoodSecurity;
+
     @GetMapping
     public RootEntryPointModel root() {
         RootEntryPointModel rootEntryPointModel = new RootEntryPointModel();
 
-        rootEntryPointModel.add(buildLinks.getLinkToCuisines("cuisines"));
-        rootEntryPointModel.add(buildLinks.getLinkToCities("cities"));
-        rootEntryPointModel.add(buildLinks.getLinkToPermissions("permissions"));
-        rootEntryPointModel.add(buildLinks.getLinkToOrders("orders"));
-        rootEntryPointModel.add(buildLinks.getLinkToRestaurants("restaurant"));
-        rootEntryPointModel.add(buildLinks.getLinkToGroups("groups"));
-        rootEntryPointModel.add(buildLinks.getLinkToUsers("users"));
-        rootEntryPointModel.add(buildLinks.getLinkToPaymentType("payment types"));
-        rootEntryPointModel.add(buildLinks.getLinkToProvinces("provinces"));
-        rootEntryPointModel.add(buildLinks.getLinkToStatistics("statistics"));
+        if(rlspFoodSecurity.hasPermissionToQueryCuisines()){
+            rootEntryPointModel.add(buildLinks.getLinkToCuisines("cuisines"));
+        }
+
+        if(rlspFoodSecurity.hasPermissionToQueryOrders()) {
+            rootEntryPointModel.add(buildLinks.getLinkToOrders("orders"));
+
+        }
+
+        if(rlspFoodSecurity.hasPermissionToQueryRestaurants()) {
+            rootEntryPointModel.add(buildLinks.getLinkToRestaurants("restaurant"));
+
+        }
+        if(rlspFoodSecurity.hasPermissionToQueryUsersGroupsPermissions()){
+            rootEntryPointModel.add(buildLinks.getLinkToGroups("groups"));
+            rootEntryPointModel.add(buildLinks.getLinkToUsers("users"));
+            rootEntryPointModel.add(buildLinks.getLinkToPermissions("permissions"));
+        }
+
+        if(rlspFoodSecurity.hasPermissionToQueryCities()) {
+            rootEntryPointModel.add(buildLinks.getLinkToCities("cities"));
+        }
+
+        if(rlspFoodSecurity.hasPermissionToQueryPaymentTypes()) {
+            rootEntryPointModel.add(buildLinks.getLinkToPaymentType("payment types"));
+        }
+
+        if(rlspFoodSecurity.hasPermissionToQueryProvinces()) {
+            rootEntryPointModel.add(buildLinks.getLinkToProvinces("provinces"));
+        }
+
+        if(rlspFoodSecurity.hasPermissionToQueryStatistics()) {
+            rootEntryPointModel.add(buildLinks.getLinkToStatistics("statistics"));
+        }
 
         return rootEntryPointModel;
     }
